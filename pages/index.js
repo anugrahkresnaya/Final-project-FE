6f415100d958f3d1e690d4e31fc684ba9c21a46d
rezadanang <rezadanang23@gmail.com>
2022-12-05 19:07:47 +0700

    add image hero and custom footer components

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,6 @@ export default function Home() {
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     <Navigation />
-    {/* <BgImage /> */}
     <Hero />
     <WhyUs />
     <Benefits />
